d8519311345ad8ac0efd8ecce26eaa9b25f4654d
Pedro Lima <emailpedrolima@gmail.com>
2022-10-12 17:02:58 -0300

    exercício 4

---
+++ ex4.js
@@ -0,0 +1,15 @@
+/*Utilizando estrutura de repetição, some os números digitados através do prompt enquanto o número for diferente de -1, quando digitado -1 pare de exibir o prompt e exiba através de um alert o resultado da soma dos números digitados.*/
+
+
+function somaNumeros(){
+    let numero = parseInt(prompt("Número:"));
+    let total = 0;
+    while (numero != -1){
+        total += numero;
+        numero = parseInt(prompt("Número:"));
+    }
+    return total;
+}
+
+
+alert(somaNumeros())
